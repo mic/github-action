2198ed6675c40aa72c0477029834c48dcf7e0dda
Michal Wlodkowski <mic@contextflow.com>
2020-01-31 15:24:31 +0100

    update action

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
 
 try {
 
-    // `who-to-greet` input defined in action metadata file
-    const pathToTasklist = core.getInput('tasklist');
-    // console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+    // Check if any Tasks are open
+    const hasOpenTasks = payload.body.match(/\[\]|\[ \]/);
+    console.log("hasOpenTasks", hasOpenTasks);
+
+    let check = {
+        name: 'task-list-completed',
+        head_sha: pr.head.sha,
+        started_at: startTime,
+        status: 'in_progress',
+        output: {
+            title: 'Outstanding tasks',
+            summary: 'Tasks still remain to be completed',
+            text: 'We check if any task lists need completing before you can merge this PR'
+        }
+    };
+
+    // all finished?
+    if (hasOpenTasks === null) {
+        check.status = 'completed';
+        check.conclusion = 'success';
+        check.completed_at = (new Date).toISOString();
+        check.output.title = 'Tasks completed';
+        check.output.summary = 'All tasks have been completed';
+    };
+
+    return github.checks.create(context.repo(check));
 } catch (error) {
     core.setFailed(error.message);
 }
